b0e2a1098c677f379c109ec6c2960a1cd47dc8f5
hasnatbrandjaws <hasnaat.brandjaws@gmail.com>
2025-02-12 22:15:30 +0500

    clean trash

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,56 +3,19 @@ import Head from "next/head";
 import React from "react";
 
 export default function Index({ HomeData }) {
-  console.log(HomeData, "HomeData");
 
   return (
     <>
       <Head>
-        <title></title>
+        <title>Home</title>
       </Head>
-      <section className="bg-themePink">
+      <section>
       <h1>
         {
           HomeData.acf.banner_section.content_main_section.main_title_column
             .main_title
         }
       </h1>
-      <h2>
-        {
-          HomeData.acf.banner_section.content_main_section.main_title_column
-            .main_title
-        }
-      </h2>
-      <h3>
-        {
-          HomeData.acf.banner_section.content_main_section.main_title_column
-            .main_title
-        }
-      </h3>
-      <h4>
-        {
-          HomeData.acf.banner_section.content_main_section.main_title_column
-            .main_title
-        }
-      </h4>
-      <h5>
-        {
-          HomeData.acf.banner_section.content_main_section.main_title_column
-            .main_title
-        }
-      </h5>
-      <h6>
-        {
-          HomeData.acf.banner_section.content_main_section.main_title_column
-            .main_title
-        }
-      </h6>
-      <p>
-        {
-          HomeData.acf.banner_section.content_main_section.main_title_column
-            .main_title
-        }
-      </p>
       </section>
     </>
   );
